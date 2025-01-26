33a568c149a3875ea9884048d632256af352d8d7
Particula173 <je.pulidog1@uniandes.edu.co>
2025-01-26 17:42:25 -0500

    asd

--- Punto_1/Punto 1.py
+++ Punto_1/Punto 1.py
@@ -1,6 +1,7 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import savgol_filter, find_peaks
 
 csv_file = 'Rhodium.csv'
 
@@ -41,6 +42,33 @@ def filtrar_por_ventanas(x, y, ventana=5, umbral=5):
     return np.array(x_limpio), np.array(y_limpio)
 
 x_limpio, y_limpio = filtrar_por_ventanas(x, y, ventana=6, umbral=3.)
+
+def punto1b(x, y):
+    window_length = 51  
+    polyorder = 3
+    background_fit = savgol_filter(y, window_length, polyorder)
+
+    isolated_peaks = y - background_fit
+
+    peaks_indices, properties = find_peaks(isolated_peaks, height=0)
+    x_peaks = x[peaks_indices]
+    y_peaks = isolated_peaks[peaks_indices]
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(x, y, label='Espectro Original', color='blue', alpha=0.5)
+    plt.plot(x, background_fit, label='Fondo Suavizado', color='orange', linestyle='--')
+    plt.plot(x, isolated_peaks, label='Espectro sin Fondo', color='green', alpha=0.7)
+    plt.scatter(x_peaks, y_peaks, color='red', label='Picos', zorder=5)
+    plt.xlabel('Wavelength (pm)')
+    plt.ylabel('Intensity')
+    plt.title('Aislamiento de los Picos de Rayos X (Filtro Savitzky-Golay)')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
+# Llamada corregida
+#punto1b(x_limpio, y_limpio)
 
 plt.figure(figsize=(10, 6))
 plt.plot(x, y, 'o-', color='b', alpha=0.5, label='Datos Originales')
