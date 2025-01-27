a7b9619f2ee6469e0953fb7a6c3b1ac652d1d308
DavidFMH1 <david_monroyhernandez@hotmail.com>
2025-01-26 19:48:06 -0500

    punto 2

--- Punto_2/Histeresis_Magnetica.py
+++ Punto_2/Histeresis_Magnetica.py
@@ -4,7 +4,9 @@ import numpy as np
 import sympy as sym
 import math
 
-ruta = 'hysteresis.dat'
+from scipy.optimize import curve_fit
+
+ruta = 'Punto_2\hysteresis.dat'
 def leer_datos(ruta)->tuple:
     archivo = open(ruta,'r')
     t = []
@@ -16,7 +18,7 @@ def leer_datos(ruta)->tuple:
         Data = []
         while len(line_1)>1:
             j = 0
-            while line_1[j+1] != ' ' and line_1[j+1] != "-":
+            while line_1[j+1] != ' ' and line_1[j+1] != "-" and j <= 5:
                 j+=1
             Data.append(float(''.join(line_1[:j+1])))
             line_1 = line_1[j+1:]
@@ -44,6 +46,58 @@ def polinomio_lagrange(x,X_s,Y_s): # Creamos una función que nos admita una var
         
     return polinomio_sum
 
-
-
+def Interpolate(x,X,Y):
     
+    Poly = 0
+    
+    for i in range(X.shape[0]):
+        Poly += polinomio_lagrange(x,X,i)*Y[i]
+        
+    return Poly
+
+a = leer_datos(ruta)
+
+'''for i in a[0]:
+    print(type(i))
+'''
+def sinusoidalfit(x,A,B):
+    y = A*np.sin(B*x) #+ C*np.cos(B*x)
+    return y
+
+guess = [2.46,2.97]
+
+param, cov = curve_fit(sinusoidalfit,a[0],a[1],p0 = guess)
+
+param = param.tolist()
+
+fit_sinu = []
+
+print(param[1]/(2*np.pi))
+
+for i in a[0]:
+    fit_sinu.append(sinusoidalfit(i,param[0],param[1]))
+
+def areaentrecurvas(B,H):
+    sum = 0
+    for i in range(len(B)-1):
+        sum += ((B[i]+B[i+1])*(H[i+1]-H[i]))/2
+
+    return sum
+
+area = areaentrecurvas(a[1],a[2])
+
+print(area)
+
+'''plt.scatter(a[0],a[1],label='B vs t' , color='r')
+plt.plot(a[0],fit_sinu)
+plt.grid()
+plt.legend()
+plt.show()'''
+'''plt.scatter(a[0],a[2],label='H vs t' , color='m')
+plt.grid()
+plt.legend()
+plt.show()'''
+plt.scatter(a[1],a[2],label='H vs B' , color='b')
+plt.grid()
+plt.legend()
+plt.show()
