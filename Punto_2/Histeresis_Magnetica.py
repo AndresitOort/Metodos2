99c5f37c01ef8fcb287c3aef5c5fcc4dff56ecfd
DavidFMH1 <david_monroyhernandez@hotmail.com>
2025-01-26 20:32:12 -0500

    punto 2

--- Punto_2/Histeresis_Magnetica.py
+++ Punto_2/Histeresis_Magnetica.py
@@ -3,10 +3,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sympy as sym
 import math
+import time
 
 from scipy.optimize import curve_fit
+from matplotlib.backends.backend_pdf import PdfPages
 
-ruta = 'Punto_2\hysteresis.dat'
+ruta = 'Punto_2\\hysteresis.dat'
 def leer_datos(ruta)->tuple:
     archivo = open(ruta,'r')
     t = []
@@ -72,7 +74,9 @@ param = param.tolist()
 
 fit_sinu = []
 
-print(param[1]/(2*np.pi))
+frecuencia =  round(param[1]/(2*np.pi),3)
+
+print(f'2.b) La frecuencia calculada es de {frecuencia} Hz.\n Se realiza un ajuste sinusoidal a los datos recibidos como parametro obteniendo asi la velocidad angular, al dividir luego por 2pi se calculo la frecuencia del campo.')
 
 for i in a[0]:
     fit_sinu.append(sinusoidalfit(i,param[0],param[1]))
@@ -85,19 +89,43 @@ def areaentrecurvas(B,H):
     return sum
 
 area = areaentrecurvas(a[1],a[2])
+print(f'La energia perdida fue de {area} Joules')
+path1 = 'C:\\Users\\david\\OneDrive\\Documentos\\Universidad\\Programas\\Metodos2\\Metodos2\\Punto_2\\histérico.pdf'
+path2 = 'C:\\Users\\david\\OneDrive\\Documentos\\Universidad\\Programas\\Metodos2\\Metodos2\\Punto_2\\energy.pdf'
 
-print(area)
+pdf = PdfPages(path1)
 
-'''plt.scatter(a[0],a[1],label='B vs t' , color='r')
+plt.figure(figsize=(10, 6))
+plt.scatter(a[0],a[1], color='r',label = f'{frecuencia} Hz')
 plt.plot(a[0],fit_sinu)
+plt.title('B vs t')
+plt.xlabel('t (s)')
+plt.ylabel('B')
 plt.grid()
 plt.legend()
-plt.show()'''
-'''plt.scatter(a[0],a[2],label='H vs t' , color='m')
+pdf.savefig()
+plt.close()
+
+
+plt.figure(figsize=(10, 6))
+plt.scatter(a[0],a[2], color='m')
+plt.title('H vs t')
+plt.xlabel('t (s)')
+plt.ylabel('H')
 plt.grid()
-plt.legend()
-plt.show()'''
-plt.scatter(a[1],a[2],label='H vs B' , color='b')
+pdf.savefig()
+plt.close()
+
+pdf.close()
+
+pdf2 = PdfPages(path2)
+
+plt.figure(figsize=(10, 6))
+plt.scatter(a[1],a[2], color='b')
+plt.title('H vs B')
+plt.xlabel('B')
+plt.ylabel('H')
 plt.grid()
-plt.legend()
-plt.show()
+pdf2.savefig()
+plt.close()
+pdf2.close()
